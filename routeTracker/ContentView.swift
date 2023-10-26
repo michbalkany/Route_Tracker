3e1209b0b2db46e8e9045485e79ad3bac864c60c
mich balkany <askmitchb@gmail.com>
2023-10-26 12:39:08 -0400

    Initial Commit

---
+++ routeTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  routeTracker
+//
+//  Created by Mich balkany on 10/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ routeTracker/routeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  routeTrackerApp.swift
+//  routeTracker
+//
+//  Created by Mich balkany on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct routeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
